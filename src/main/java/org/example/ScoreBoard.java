56e276af13183b5c1f83d98febb67c090bbb6b9a
bartlomiejgluch <bartlomiejgluch@gmail.com>
2025-03-07 13:43:15 +0100

    Add ScoreBoard class

---
+++ src/main/java/org/example/ScoreBoard.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.*;
+
+public class ScoreBoard {
+
+    private final Map<UUID, Match> onGoingMatches = new HashMap<>();
+    private final List<Match> finishedMatches = new ArrayList<>();
+
+    public void startGame(Match match) {
+        UUID id = match.getMatchId();
+        if (onGoingMatches.containsKey(id)) {
+            throw new IllegalArgumentException("Match with this ID already exists: " + match.getMatchId());
+        }
+        onGoingMatches.put(id, match);
+    }
+
+    public void finishGame(UUID matchId) {
+        Match match = Optional.ofNullable(onGoingMatches.remove(matchId))
+                .orElseThrow(() -> new IllegalArgumentException("Match with this ID does not exist: " + matchId));
+        finishedMatches.add(match);
+    }
+
+    public void updateScore(UUID matchId, int homeScore, int awayScore) {
+        Optional.ofNullable(onGoingMatches.get(matchId))
+                .orElseThrow(() -> new IllegalArgumentException("Cannot update. Match with this ID does not exist: " + matchId))
+                .updateScore(homeScore, awayScore);
+
+    }
+
+    public List<Match> getFinishedMatchesSorted() {
+        return finishedMatches.stream()
+                .sorted(
+                        Comparator.comparingInt(Match::getTotalScore).reversed()
+                                .thenComparing(Match::getMatchTime).reversed()
+                )
+                .toList();
+    }
+}
